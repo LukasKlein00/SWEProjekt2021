620949f8c4e0209a7a3194073b048d17b2107d56
Weofi <weofi@gmx.de>
2021-03-28 21:14:56 +0200

    added files

--- Infowebsite/src/app/home/home.component.ts
+++ Infowebsite/src/app/home/home.component.ts
@@ -9,15 +9,47 @@ export class HomeComponent implements OnInit {
 
   password = 'mudcakemudcake';
 
+  files1 = [
+    'Projektplan_Gruppe3_Version0.2.pdf',
+    'Projektstrukturplan.pdf',
+    'Risikoplanung_Gruppe3_Version 0.1.pdf',
+    'Rolleneinteilung_Gruppe3.pdf',
+    'Statusbericht1_Gruppe3.pdf',
+    'Arbeitspakete,Kapazitäten.xlsx',
+    'Zeitaufwandserfassung_Woche1_Gruppe3.xlsx',
+    'Timeline.png'
+  ];
+
+  files2 = [
+    'Komponentendiagramm_WrittenRealms.pdf',
+    'Recherchebericht_Gruppe3.pdf',
+    'Statusbericht2_Gruppe3.pdf',
+    'UseCase_WrittenRealms_CreateDungeon.pdf',
+    'UseCase_WrittenRealms_PlayDungeon.pdf',
+    'UseCase_WrittenRealms_Übersicht.pdf',
+    'Zeitaufwandserfassung_Woche2_Gruppe3.xlsx',
+  ];
+
+  files3 = [
+    'Designbeschreibung_MUDCAKE.pdf',
+    'Dokumentenvorlage_MUDCAKE.pdf',
+    'Glossar_MUDCAKE_Version2.pdf',
+    'Lasten_Pflichtenheftvorlage_MUDCAKE.pdf',
+    'Lastenheft_MUDCAKE_Version2.pdf',
+    'Qualitätssicherungskonzept_MUDCAKE.pdf',
+    'Statusbericht_Woche3_MUDCAKE.pdf',
+    'Aufwandserfassung_Woche3_MUDCAKE.xlsx',
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  psw(pfad: string) {
+  psw(pfad: string, ordner: string) {
     if (prompt('Enter Password To Download Doc') === this.password) {
       const d = document.createElement('a');
-      d.setAttribute('href', 'assets/' + pfad);
+      d.setAttribute('href', 'assets/' + ordner + '/' + pfad);
       d.setAttribute('download', pfad);
       d.click();
     } else {
