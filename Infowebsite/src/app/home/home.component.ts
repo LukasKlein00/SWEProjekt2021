b78dc5a7d79a2c1bca1b72a748cfe168ec1e5eec
Weofi <weofi@gmx.de>
2021-04-08 11:40:36 +0200

    private and public files

--- Infowebsite/src/app/home/home.component.ts
+++ Infowebsite/src/app/home/home.component.ts
@@ -15,19 +15,24 @@ export class HomeComponent implements OnInit {
     'Projektstrukturplan.pdf',
     'Risikoplanung_Gruppe3_Version 0.1.pdf',
     'Rolleneinteilung_Gruppe3.pdf',
+    'Timeline.png',
+  ];
+  files1p = [
     'Statusbericht1_Gruppe3.pdf',
     'Arbeitspakete,Kapazitäten.xlsx',
     'Zeitaufwandserfassung_Woche1_Gruppe3.xlsx',
-    'Timeline.png'
   ];
+
 
   files2 = [
     'Komponentendiagramm_WrittenRealms.pdf',
     'Recherchebericht_Gruppe3.pdf',
-    'Statusbericht2_Gruppe3.pdf',
     'UseCase_WrittenRealms_CreateDungeon.pdf',
     'UseCase_WrittenRealms_PlayDungeon.pdf',
     'UseCase_WrittenRealms_Übersicht.pdf',
+  ];
+  files2p = [
+    'Statusbericht2_Gruppe3.pdf',
     'Zeitaufwandserfassung_Woche2_Gruppe3.xlsx',
   ];
 
@@ -38,14 +43,18 @@ export class HomeComponent implements OnInit {
     'Lasten_Pflichtenheftvorlage_MUDCAKE.pdf',
     'Lastenheft_MUDCAKE_Version2.pdf',
     'Qualitätssicherungskonzept_MUDCAKE.pdf',
+  ];
+  files3p = [
     'Statusbericht_Woche3_MUDCAKE.pdf',
     'Aufwandserfassung_Woche3_MUDCAKE.xlsx',
   ];
 
   files4 = [
-    'Statusbericht_Woche4_MUDCAKE.pdf',
     'Glossar_MUDCAKE_Version2.pdf',
     'Pflichtenheft_MUDCAKE_Version1.1.pdf',
+  ];
+  files4p = [
+    'Statusbericht_Woche4_MUDCAKE.pdf',
     'Aufwandserfassung_Woche4_MUDCAKE.xlsx',
   ];
 
