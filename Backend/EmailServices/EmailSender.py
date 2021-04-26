19dde5c9252e30e9db202e2af1fc1a72a328e249
LukasKlein00 <lukasklei00@gmail.com>
2021-04-26 21:31:40 +0200

    Added Docstrings

--- Backend/EmailServices/EmailSender.py
+++ Backend/EmailServices/EmailSender.py
@@ -6,18 +6,32 @@ from Backend.EmailServices.fileReader import fileReader
 
 
 class EmailSender:
+    """Basic class for interaction to User via Email"""
     def __init__(self, userEmail, token: str = None):
+        """Constructor for Email Sender to initiate needed parameters
+
+        :param userEmail: Email of the recipient - String
+        :param token: Token to verify User after email confirmation - String
+        """
         self.userEmail = userEmail
         self.token = token
         self.msg = EmailMessage()
         self.email = "mudcakegame@gmail.com"
 
     def __sendViaServerContext(self, msg: EmailMessage):
+        """Method to send emails via google gMail
+
+        :param msg: preconfigured message in external method - EmailMessage
+        """
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context()) as server:
             server.login(self.email, bytes(b'$9PLnJ5NsB#!').decode('utf8', 'strict'))
             server.send_message(msg)
 
     def sendEmail(self, mType: messageType):
+        """Prepares email with given context and initializes sending
+
+        :param mType: type of message to be send - MessageType
+        """
         if mType == messageType.registration:
             contentReader = fileReader("confirmationEmailTemplates/content")
             self.msg.set_content(contentReader.read())
