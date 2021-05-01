7a8035d978261a94995523db8621e3b58e647ee3
LukasKlein00 <lukasklei00@gmail.com>
2021-05-01 12:06:52 +0200

    Refactored ActiveDungeon.py and DungeonData.py

--- Backend/DungeonPackage/ActiveDungeon.py
+++ Backend/DungeonPackage/ActiveDungeon.py
@@ -6,5 +6,39 @@ from DungeonPackage.Class import Class as Class
 
 
 class ActiveDungeon:
-    def __init__(self, userIDs: [int], characterIDs: [int], rooms: [Room], npcs: [Npc]):
-        return
+    def __init__(self, userIDs: [int], characterIDs: [int], rooms: [Room], npcs: [Npc], items: [Item], races: [Race],
+                 classes: [Class]):
+        self.userIDs = userIDs
+        self.characterIDs = characterIDs
+        self.rooms = rooms
+        self.npcs = npcs
+        self.items = items
+        self.races = races
+        self.classes = classes
+
+    def addItem(self):
+        raise NotImplementedError
+
+    def addRace(self):
+        raise NotImplementedError
+
+    def isDungeonMasterInGame(self):
+        raise NotImplementedError
+
+    def addRoom(self):
+        raise NotImplementedError
+
+    def addClass(self):
+        raise NotImplementedError
+
+    def loadData(self):
+        raise NotImplementedError
+
+    def changeRaceVisibility(self):
+        raise NotImplementedError
+
+    def changeClassVisibility(self):
+        raise NotImplementedError
+
+    def moveCharacter(self):
+        raise NotImplementedError
--- Backend/DungeonPackage/DungeonData.py
+++ Backend/DungeonPackage/DungeonData.py
@@ -1,3 +1,34 @@
+from DungeonPackage.AccessList import AccessList as AccessList
+
+
 class DungeonData:
-    def __init__(self):
-        return
+    def __init__(self, dungeonId: int, dungeonMasterID: int, maxPlayers: int, name: str, description: str,
+                 private: bool, accessList: AccessList):
+        self.dungeonId = dungeonId
+        self.dungeonMasterID = dungeonMasterID
+        self.maxPlayers = maxPlayers
+        self.name = name
+        self.description = description
+        self.private = private
+        self.accessList = accessList
+
+    def isDungeonMasterIn(self):
+        raise NotImplementedError
+
+    def addRoom(self):
+        raise NotImplementedError
+
+    def addItemToRoom(self):
+        raise NotImplementedError
+
+    def addNpcToRoom(self):
+        raise NotImplementedError
+
+    def loadData(self):
+        raise NotImplementedError
+
+    def isPrivate(self):
+        raise NotImplementedError
+
+    def overwriteDungeonMasterID(self):
+        raise NotImplementedError
