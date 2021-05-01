f8630f204cd825d1b8057be0da1cf74af470fdb2
thom-zim <thomaszimmermann71@gmail.com>
2021-05-01 13:12:17 +0200

    Refactored ActiveDungeon.py and DungeonData.py

--- Backend/FrontendHandler/HttpHandler.py
+++ Backend/FrontendHandler/HttpHandler.py
@@ -1,3 +1,92 @@
-class HttpHandler:
-    def __init__(self):
-        return
+from http.server import BaseHTTPRequestHandler
+import json
+from DatabaseHandler.DatabaseHandler import *
+from DatabaseHandler.User import *
+from DungeonPackage.DungeonData import *
+from DungeonPackage.ActiveDungeon import *
+import mysql
+
+
+class HTTPHandler(BaseHTTPRequestHandler):
+    mDBHandler = DatabaseHandler(mysql.connector.connect(
+        host="193.196.53.67",
+        port="1189",
+        user="jack",
+        password="123123"
+    ))
+
+    # übermittelt Einstellungen "Headers" des Requests
+    def _set_response(self, code: int = 200):
+        self.send_response(code)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Methods', 'POST, GET')
+        self.send_header('Access-Control-Allow-Headers', 'Authorization, Content-Type')
+        self.end_headers()
+
+    # OPTIONS Request: Wird vor jeder Request ausgeführt, um zu checken, ob die Request erlaubt ist
+    def do_OPTIONS(self):
+        self._set_response()
+
+        # GET Request: "Wird verwendet wenn Daten ausgegeben werden sollen"
+
+    def do_GET(self):
+        pass
+
+    # POST Request: "Wird verwendet wenn Daten angenommen werden sollen"
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])  # <--- Größe der Daten
+        post_data_raw = self.rfile.read(content_length)  # <--- Erfasst die Daten
+        try:
+            data = json.loads(post_data_raw)  # <--Daten als JSON-Objekt
+            print(data)
+        except:
+            data = None
+
+        if self.path == '/register':
+            self._set_response()
+            newUser = User(userID=data['userID'], firstName=data['firstName'], lastName=data['lastName'],
+                           eMail=data['email'], userName=data['username'], password=data['password'],
+                           confirmation=False)
+            self.mDBHandler.registerUser(newUser)
+
+            # Antwort senden? self.wfile.write(json.dumps("moin").encode(encoding='utf_8'))
+
+        if self.path == '/login':
+            self._set_response()
+            newUser = User(userName=data['username'], password=data['password'])
+            returnedUser = self.mDBHandler.loginUser(newUser)
+            if returnedUser:
+                response = {
+                    'username': returnedUser.userName,
+                    'userID': returnedUser.userID
+                }
+                self.wfile.write(json.dumps(response).encode(encoding='utf_8'))
+            else:
+                self._set_response(400)
+
+        if self.path == '/getMyDungeons':
+            self._set_response()
+            response = self.mDBHandler.getDungeonByID(data)
+            self.wfile.write(json.dumps(response).encode(encoding='utf_8'))
+
+        if self.path == '/saveDungeon':
+            self._set_response()
+            newDungeon = DungeonData(dungeonDescription=data['dungeonDescription'], dungeonName=data['dungeonName'],
+                                 dungeonID=data['dungeonID'], maxPlayers=data['maxPlayers'], private=data['private'],
+                                 dungeonMasterID=data['dungeonMasterID'])
+            dungeonID = self.mDBHandler.saveOrUpdateDungeon(newDungeon)
+            # noch items und so abspeichern
+
+        if self.path == '/deleteDungeon':
+            self._set_response()
+            self.mDBHandler.deleteDungeonByID(data)
+
+        if self.path == '/deleteUser':
+            self._set_response()
+            self.mDBHandler.deleteUserByID(data)
+
+        if self.path == '/copyDungeon':
+            print(data)
+            self._set_response()
+            # self.mDBHandler.copyDungeon(data)
--- Backend/FrontendHandler/WebsocketHandler.py
+++ Backend/FrontendHandler/WebsocketHandler.py
@@ -1,3 +1,29 @@
-class WebsocketHandler:
+import asyncio
+import websockets
+import json
+
+
+class WebSocketHandler:
     def __init__(self):
-        return
+        self.allConnections = set()
+
+
+    async def handle(self,websocket, path):
+        try:
+            #jeder Socket/Spieler trägt sich in Set ein
+            self.allConnections.add(websocket)
+
+            #immer wenn die Instanz eine Nachricht sendet...
+            async for msg in websocket:
+                data = json.loads(msg)
+                print(f"received: {data}")
+
+                replyMessage = {
+                    'content': 'hallo wie gehts'
+                }
+
+                await websocket.send(json.dumps(replyMessage))
+
+        finally:
+            #Beim Disconnecten trägt sich die Instanz aus dem Set aus
+            self.allConnections.discard(websocket)
