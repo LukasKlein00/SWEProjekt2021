091e1ae50471faa5e92749e311404fb77fa4b687
Bobbolus <robs.fendrich@ecotext.de>
2021-05-02 14:37:10 +0200

    takit

--- Backend/FrontendHandler/HttpHandler.py
+++ Backend/FrontendHandler/HttpHandler.py
@@ -108,6 +108,8 @@ class HTTPHandler(BaseHTTPRequestHandler):
 
         if self.path == '/reset':
             self._set_response()
+            print('data')
+            print(data)
             isPasswordChanged = self.AccManager.changePasswordInDatabase(UserID=data['userID'], Password=data['password'])
             if not isPasswordChanged:
                 self._set_response(400)
