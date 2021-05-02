29d8c62d7c92fc41e64ab0349d084ec28d99f9d6
Weofi <weofi@gmx.de>
2021-05-02 12:27:18 +0200

    improved password reset page

--- Client/src/app/reset/reset.component.ts
+++ Client/src/app/reset/reset.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
 import { AuthentificationService } from '../services/authentification.service';
 
 @Component({
@@ -8,35 +10,53 @@ import { AuthentificationService } from '../services/authentification.service';
   styleUrls: ['./reset.component.scss']
 })
 export class ResetComponent implements OnInit {
-  
+  resetForm: FormGroup;
+  loading = false;
+  submitted = false;
 
   constructor(
+    private formBuilder: FormBuilder,
     private route: ActivatedRoute,
-    private authentificationService: AuthentificationService,
+    private authenticationService: AuthentificationService,
     private router: Router,
   ) { }
 
   token: string;
-  psw: string;
-  error;
 
 
   ngOnInit(): void {
-    
     this.route.queryParams.subscribe(p => {
       this.token = p['token'];
       console.log(p)
-    })
+    });
+    this.resetForm = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
-  reset() {
-    if (this.psw && this.psw.length >= 6) {
-      this.authentificationService.confirm(this.token).subscribe(res => {
-        this.router.navigateByUrl('');
-      });
-    } else {
-      this.error = "Password is too short"
+  get f() { return this.resetForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.resetForm.invalid) {
+      return;
     }
+
+    this.loading = true;
+    this.authenticationService.confirm(this.resetForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.resetForm.get('password').setErrors({
+            fail: true
+          });
+          this.loading = false;
+        });
   }
 
 }
