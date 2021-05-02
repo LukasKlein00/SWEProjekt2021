75832e451619db14d4cc8c853468b24a109926e3
Bobbolus <robs.fendrich@ecotext.de>
2021-05-02 14:56:40 +0200

    takit

--- Backend/FrontendHandler/HttpHandler.py
+++ Backend/FrontendHandler/HttpHandler.py
@@ -4,7 +4,6 @@ from http.server import BaseHTTPRequestHandler
 import mysql
 
 from BackendServices.AccountManager import AccountManager
-from BackendServices.DungeonManager import DungeonManager
 from DatabaseHandler.DatabaseHandler import *
 from DatabaseHandler.User import *
 from DungeonPackage.ActiveDungeon import *
@@ -81,13 +80,12 @@ class HTTPHandler(BaseHTTPRequestHandler):
 
         if self.path == '/saveDungeon':
             self._set_response()
-            """newDungeon = DungeonData(dungeonDescription=data['dungeonDescription'], dungeonName=data['dungeonName'],
+            newDungeon = DungeonData(dungeonDescription=data['dungeonDescription'], dungeonName=data['dungeonName'],
                                      dungeonID=data['dungeonID'], maxPlayers=data['maxPlayers'],
                                      private=data['private'],
-                                     dungeonMasterID=data['dungeonMasterID'])"""
-            dungeon_manager = DungeonManager(data)
-            dungeon_id = dungeon_manager.write_dungeon_to_database()
-            self.wfile.write(json.dumps(dungeon_id).encode(encoding='utf_8'))
+                                     dungeonMasterID=data['dungeonMasterID'])
+            dungeonID = self.mDBHandler.saveOrUpdateDungeon(newDungeon)
+            # noch items und so abspeichern
 
         if self.path == '/deleteDungeon':
             self._set_response()
