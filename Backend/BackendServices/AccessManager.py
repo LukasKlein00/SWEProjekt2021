91e273a984bfc310b86bb7e8a39400757876ea9f
LukasKlein00 <lukasklei00@gmail.com>
2021-05-03 19:03:24 +0200

    Prepared AccessManager.py

---
+++ Backend/BackendServices/AccessManager.py
@@ -0,0 +1,22 @@
+from DatabaseHandler.DatabaseHandler import *
+
+
+class AccessManager:
+    def __init__(self):
+        self.mDBHandler = DatabaseHandler()
+
+    def __user_status_on_access_list(self, userID: str, dungeonID: str):
+        ret = self.mDBHandler.user_status_on_access_list(userID, dungeonID)
+        if ret != None:
+            return bool(ret[0])
+        else:
+            return None
+
+    def send_access_request_to_dm(self):
+        raise NotImplementedError
+
+    def join_user_to_white_list(self, userID: str, dungeonID: str):
+        raise NotImplementedError
+
+    def join_user_to_black_list(self, userID: str, dungeonID: str):
+        raise NotImplementedError
