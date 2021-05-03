b2ff85af714e2a419742b6c856c760a919db5a56
Weofi <weofi@gmx.de>
2021-05-03 20:32:38 +0200

    change styling

--- Client/src/app/builder/builder.component.ts
+++ Client/src/app/builder/builder.component.ts
@@ -14,7 +14,6 @@ export class BuilderComponent implements OnInit {
 
   loading = false;
   privateSlider = false;
-  maxPlayerOptions = [3, 4, 5, 6, 7, 8, 9, 10]
   requests: requestForMaster[] = [
     {
       request: 'kill Spider',
