69bfd9a838ed4693db4738d5d5bedb26664a6992
Weofi <weofi@gmx.de>
2021-05-04 10:46:47 +0200

    start resonsive design

--- Client/src/app/home/home.component.ts
+++ Client/src/app/home/home.component.ts
@@ -11,8 +11,8 @@ export class HomeComponent implements OnInit {
 
   loading = false;
   availableMUDs: Dungeon[] = [{
-    dungeonName: "Jack's mega cooler TestDungeon",
-    dungeonDescription: 'Es war einmal vor langer Zeit ein nettes SWE Projekt, welches dafür sorgt, das mal ganz viel Erfahrung sammelt und sein Leben einfach genießt',
+    dungeonName: "Jack's mega cooler Dungeon",
+    dungeonDescription: "We're no strangers to love. You know the rules and so do I. A full commitment's what I'm thinking of. You wouldn't get this from any other guy",
     dungeonID: "1",
     maxPlayers: 10,
     currentPlayers: 5,
