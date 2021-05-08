55b6e368ae230bfa76a227074f391fa9ae7c11b7
Bobbolus <robs.fendrich@ecotext.de>
2021-05-08 15:50:17 +0200

    Unittest_iniital

---
+++ Backend/BackendServices/Test_AccessManager.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test___user_status_on_access_list(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
