dd9098644c40f0c3a5cd09e2411694a2e6ebf4ad
LukasKlein00 <lukasklei00@gmail.com>
2021-05-09 11:12:53 +0200

    Mergein stuff

--- Backend/BackendServices/DungeonManager.py
+++ Backend/BackendServices/DungeonManager.py
@@ -38,6 +38,7 @@ from json import JSONEncoder as foreignEncoder
 
 from DatabaseHandler.DatabaseHandler import DatabaseHandler
 from DungeonPackage.ActiveDungeon import ActiveDungeon
+from DungeonPackage.Character import Character
 from DungeonPackage.Class import Class
 from DungeonPackage.DungeonData import DungeonData
 from DungeonPackage.Item import Item
@@ -250,6 +251,19 @@ class DungeonManager:
                 self.db_handler.write_room_to_database(room=room, dungeon_id=self.managed_dungeon.dungeon_id)
             except IOError:
                 pass
+
+    def write_character_to_database(self, character: Character):
+        """ writes a given character to the database
+
+        Args:
+            character (Character): character to be written to the database
+
+        Returns: void
+        """
+        try:
+            self.db_handler.write_character_to_database(character, character.dungeon_id)
+        except IOError:
+            pass
 
     def __write_items_to_database(self):
         """
