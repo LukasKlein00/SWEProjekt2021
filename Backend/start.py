83aaa2b592536ea77b268a056a99eb0512f7fa5a
LukasKlein00 <lukasklei00@gmail.com>
2021-05-13 17:56:24 +0200

    Added Header to everything

--- Backend/start.py
+++ Backend/start.py
@@ -1,11 +1,41 @@
+#!/usr/bin/env python
+__author__ = "Jack Drillisch"
+__copyright__ = "Copyright 2021, The MUDCake Project"
+__credits__ = "Hauke Presig, Jack Drillisch, Jan Gruchott, Lukas Klein, Robert Fendrich, Thomas Zimmermann"
+
+__license__ = """MIT License
+
+                     Copyright (c) 2021 MUDCake Project
+
+                     Permission is hereby granted, free of charge, to any person obtaining a copy
+                     of this software and associated documentation files (the "Software"), to deal
+                     in the Software without restriction, including without limitation the rights
+                     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                     copies of the Software, and to permit persons to whom the Software is
+                     furnished to do so, subject to the following conditions:
+
+                     The above copyright notice and this permission notice shall be included in all
+                     copies or substantial portions of the Software.
+
+                     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                     SOFTWARE."""
+
+__version__ = "1.0.0"
+__maintainer__ = "Jack Drillisch"
+__email__ = "mudcake@gmail.com"
+__status__ = "Development"
+
 import logging
 import socket
 
 import eventlet
 
 from FrontendHandler.WebsocketHandler import SocketIOHandler
-import websockets
-import asyncio
 from http.server import HTTPServer
 from FrontendHandler.HttpHandler import HTTPHandler
 from threading import Thread
@@ -23,13 +53,6 @@ def start_HTTP():
 
 
 def start_WS():
-    """ws = WebSocketHandler
-    loop = asyncio.new_event_loop()
-    print('Starting WebsocketServer... \n')
-    start_server = websockets.serve(ws.handle, '', 1187, loop=loop)
-    print('WebsocketServer is running!')
-    loop.run_until_complete(start_server)
-    loop.run_forever()"""
     ip = ""
     socketio = SocketIOHandler()
     host_name = socket.gethostname()
