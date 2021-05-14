3eac8db11ff8b3c1052bc3082d28ed992bc36dee
LukasKlein00 <lukasklei00@gmail.com>
2021-05-14 21:28:51 +0200

    Added discovered room functionality

--- Backend/FrontendHandler/WebsocketHandler.py
+++ Backend/FrontendHandler/WebsocketHandler.py
@@ -44,14 +44,14 @@ from DungeonDirector.ActiveDungeonHandler import ActiveDungeonHandler
 import socketio
 import random
 
-
 # TODO: user access management
 from DungeonPackage.Race import Race
 
 
 class SocketIOHandler:
     def __init__(self):
-        self.sio = socketio.Server(cors_allowed_origins='*', async_mode='eventlet', client_manager=socketio.BaseManager())
+        self.sio = socketio.Server(cors_allowed_origins='*', async_mode='eventlet',
+                                   client_manager=socketio.BaseManager())
         self.sio.logger.disabled = True
         self.app = socketio.WSGIApp(self.sio)
         self.activeDungeonHandler = ActiveDungeonHandler()
@@ -74,7 +74,7 @@ class SocketIOHandler:
                 self.sio.emit("on_join_request_answer", answer, to=socket)
             count = self.activeDungeonHandler.user_count_in_dungeon[session['dungeonID']]
             self.activeDungeonHandler.user_count_in_dungeon[session['dungeonID']] = count + 1
-            #region UpdateDungeon
+            # region UpdateDungeon
             dungeon_data_list = []
             for dungeon_ID in self.activeDungeonHandler.active_dungeon_ids:
                 dungeon_data = DungeonData(dungeon_id=dungeon_ID).load_data(dungeon_id=dungeon_ID)
@@ -88,7 +88,7 @@ class SocketIOHandler:
                 dungeon_data_list.append(dungeon_dict)
                 print(colored(dungeon_dict, 'green'))
             self.sio.emit('make_dungeon_available', json.dumps(dungeon_data_list), broadcast=True)
-            #endregion
+            # endregion
 
         @self.sio.event
         def on_login(sid, data):
@@ -102,12 +102,11 @@ class SocketIOHandler:
 
         @self.sio.event
         def on_home(sid):
-            #region UpdateDungeon
+            # region UpdateDungeon
             dungeon_data_list = []
 
             for dungeon_ID in self.activeDungeonHandler.active_dungeon_ids:
                 if self.activeDungeonHandler.sid_of_dungeon_master[dungeon_ID] != sid:
-
                     dungeon_data = DungeonData(dungeon_id=dungeon_ID).load_data(dungeon_id=dungeon_ID)
 
                     dungeon_dict = {"dungeonID": dungeon_data.dungeon_id,
@@ -122,7 +121,7 @@ class SocketIOHandler:
                     print(colored(dungeon_dict, 'green'))
             self.sio.emit('make_dungeon_available', json.dumps(dungeon_data_list), to=sid)
             print(colored("publish successful", 'green'))
-            #endregion
+            # endregion
 
         @self.sio.event
         def connect(sid, environ, data):
@@ -130,7 +129,7 @@ class SocketIOHandler:
             dungeon_data_list = []
             print(colored(f"Dungeon Data List: {dungeon_data_list}", 'red'))
             print(colored(f"Dungeon Handler List: {self.activeDungeonHandler.active_dungeon_ids}", 'red'))
-            #region UpdateDungeon
+            # region UpdateDungeon
             for dungeon_ID in self.activeDungeonHandler.active_dungeon_ids:
                 if self.activeDungeonHandler.sid_of_dungeon_master[dungeon_ID] != sid:
                     dungeon_data = DungeonData(dungeon_id=dungeon_ID).load_data(dungeon_id=dungeon_ID)
@@ -145,7 +144,7 @@ class SocketIOHandler:
             if len(dungeon_data_list) != 0:
                 self.sio.emit('make_dungeon_available', json.dumps(dungeon_data_list), broadcast=True)
                 print(colored("publish successful", 'green'))
-            #endregion
+            # endregion
 
         # TODO: mit jack klären, dass aktive dungeons die vom dungeonmaster verlassen wurden beim dungeon master als
         # aktiv gekennzeicnet werden und dungeon ein beenden button bekommen
@@ -157,7 +156,7 @@ class SocketIOHandler:
             if sid in self.activeDungeonHandler.sid_of_dungeon_master.values():
                 dungeon_data_list = []
                 self.activeDungeonHandler.dungeon_leave(self.sio.get_session(sid)['dungeonID'])
-                #region UpdateDungeon
+                # region UpdateDungeon
                 if self.activeDungeonHandler.active_dungeon_ids.__len__() != 0:
                     for dungeon_ID in self.activeDungeonHandler.active_dungeon_ids:
                         dungeon_data = DungeonData(dungeon_id=dungeon_ID).load_data(dungeon_id=dungeon_ID)
@@ -174,15 +173,15 @@ class SocketIOHandler:
                     print(colored("publish successful", 'green'))
                 else:
                     self.sio.emit('make_dungeon_available', json.dumps([]), broadcast=True)
-                #endregion
+                # endregion
             print('disconnect: ', sid)
 
             try:
                 session = self.sio.get_session(sid)
                 count = self.activeDungeonHandler.user_count_in_dungeon[session['dungeonID']]
-                self.activeDungeonHandler.user_count_in_dungeon[session['dungeonID']] = count-+ 1
+                self.activeDungeonHandler.user_count_in_dungeon[session['dungeonID']] = count - + 1
                 dungeon_data_list = []
-                #region UpdateDungeon
+                # region UpdateDungeon
                 for dungeon_ID in self.activeDungeonHandler.active_dungeon_ids:
                     dungeon_data = DungeonData(dungeon_id=dungeon_ID).load_data(dungeon_id=dungeon_ID)
                     dungeon_dict = {"dungeonID": dungeon_data.dungeon_id,
@@ -195,7 +194,7 @@ class SocketIOHandler:
                     dungeon_data_list.append(dungeon_dict)
                     print(colored(dungeon_dict, 'green'))
                 self.sio.emit('make_dungeon_available', json.dumps(dungeon_data_list), broadcast=True)
-                #endregion
+                # endregion
             except IOError:
                 pass
 
@@ -210,12 +209,12 @@ class SocketIOHandler:
         def send_character_config(sid, character):
             dungeon_id = character["dungeonID"]
 
-            #region Adding user to starting room in active dungeon
+            # region Adding user to starting room in active dungeon
             all_start_rooms = self.dungeon_manager.get_start_rooms_in_dungeon(dungeon_id=dungeon_id)
             starting_room = random.choice(all_start_rooms)
-            #endregion
+            # endregion
 
-            #region Adding user to starting room in backend
+            # region Adding user to starting room in backend
             current_dungeon = ActiveDungeon(
                 self.activeDungeonHandler.active_dungeons[dungeon_id]['active_dungeon_object'])
             current_dungeon.load_rooms(dungeon_id)
@@ -226,8 +225,10 @@ class SocketIOHandler:
             # TODO: inventory (class startitem)
             character_obj = Character(room_id=starting_room['roomID'], life_points=character["health"],
                                       name=character["name"], description=character["description"],
-                                      class_obj=Class(class_id=character["class"]["classID"]), race=Race(race_id=character["race"]["raceID"]),
-                                      user_id=character["userID"], dungeon_id=dungeon_id, character_id=str(uuid.uuid4()))
+                                      class_obj=Class(class_id=character["class"]["classID"]),
+                                      race=Race(race_id=character["race"]["raceID"]),
+                                      user_id=character["userID"], dungeon_id=dungeon_id,
+                                      character_id=str(uuid.uuid4()))
             session["character"] = character_obj
 
             for room in all_room_objects_in_dungeon[1:]:
@@ -236,25 +237,29 @@ class SocketIOHandler:
                     room.user_ids.append(character["userID"])
                     character = self.sio.get_session(sid)['character']
                     character.room_id = room.room_id
-            #endregion
+                    character.discovered_rooms.append(room.room_id)
+                    character.discovered_rooms_to_database()
+            # endregion
 
             self.dungeon_manager.write_character_to_database(character_obj)
 
-
-            #region Adding class startitem to user inventory when first joining
+            # region Adding class startitem to user inventory when first joining
             item = self.dungeon_manager.get_item_by_class_id(character["class"]["classID"])
             character_obj.add_item_to_inventory(item.item_id)
-            #endregion
-
-            # self.sio.emit('character_joined_room',
-            #               json.dumps({'startRoom': starting_room, 'allOtherRoomsToLoad': all_rooms_in_dungeon}), sid=sid)
+            # endregion
 
         @self.sio.event
-        def character_joins_room(sid, character):
-            raise NotImplementedError
+        def character_joins_dungeon(sid, character):
+            character_obj = Character(life_points=character["health"],
+                                      name=character["name"], description=character["description"],
+                                      user_id=character["userID"], dungeon_id=character['dungeonID'])
+            character_obj.load_discovered_rooms_from_database()
+            all_discovered_rooms_ids_by_character = character_obj.discovered_rooms
+            all_discovered_rooms = self.dungeon_manager.get_data_for_room_list(all_discovered_rooms_ids_by_character,
+                                                                               character['dungeonID'])
+            self.sio.emit('character_joined_room', json.dumps(all_discovered_rooms), sid)
 
-
-        @ self.sio.event
+        @self.sio.event
         def join_dungeon(sid, data):  # Data = Dict aus DungeonID und UserID/Name
             self.sio.enter_room(sid, data['dungeonID'])
             session = self.sio.get_session(sid)
@@ -266,7 +271,7 @@ class SocketIOHandler:
                 count = self.activeDungeonHandler.user_count_in_dungeon[data['dungeonID']]
                 self.activeDungeonHandler.user_count_in_dungeon[data['dungeonID']] = count + 1
                 dungeon_data_list = []
-                #region UpdateDungeon
+                # region UpdateDungeon
                 for dungeon_ID in self.activeDungeonHandler.active_dungeon_ids:
                     dungeon_data = DungeonData(dungeon_id=dungeon_ID).load_data(dungeon_id=dungeon_ID)
                     dungeon_dict = {"dungeonID": dungeon_data.dungeon_id,
@@ -280,7 +285,7 @@ class SocketIOHandler:
                     print(colored(dungeon_dict, 'green'))
                 self.sio.emit('make_dungeon_available', json.dumps(dungeon_data_list), broadcast=True)
                 self.sio.emit("on_join_request_answer", json.dumps(True), to=sid)
-                #endregion
+                # endregion
             elif user_status is False:
                 print("wat isn jetzt passiert")
                 self.sio.emit("on_join_request_answer", json.dumps(False), to=sid)
@@ -308,7 +313,7 @@ class SocketIOHandler:
             session["dungeonID"] = data
             self.activeDungeonHandler.user_count_in_dungeon[data] = 0
             #########################
-            #region UpdateDungeon
+            # region UpdateDungeon
             dungeon_data_list = []
             for dungeon_ID in self.activeDungeonHandler.active_dungeon_ids:
                 dungeon_data = DungeonData(dungeon_id=dungeon_ID).load_data(dungeon_id=dungeon_ID)
@@ -321,7 +326,7 @@ class SocketIOHandler:
             if len(dungeon_data_list) != 0:
                 self.sio.emit('make_dungeon_available', json.dumps(dungeon_data_list), broadcast=True, skip_sid=sid)
                 print(colored("publish successful", 'green'))
-            #endregion
+            # endregion
 
         # TODO: ausprobieren JACK!!!
         @self.sio.event
@@ -385,7 +390,7 @@ class SocketIOHandler:
                         else:
                             self.sio.emit('no_room_in_this_direction', json.dumps(False), to=sid)
 
-             # ist Raum in gewünschte richtung offen?
+            # ist Raum in gewünschte richtung offen?
             # ist da ein Raum?
             # if so -> move character id from roomID(alt) to roomID(neu)
             # geb zurück neue koordinaten für frontend
