e1b9cda45cf8dfc11c55e80495dce785792ea27b
Weofi <weofi@gmx.de>
2021-05-18 09:41:56 +0200

    fixing tests

--- Client/src/app/confirm/confirm.component.spec.ts
+++ Client/src/app/confirm/confirm.component.spec.ts
@@ -3,16 +3,19 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
 
 import { ConfirmComponent } from './confirm.component';
 
 describe('ConfirmComponent', () => {
   let component: ConfirmComponent;
   let fixture: ComponentFixture<ConfirmComponent>;
+  const config: SocketIoConfig = { url: environment.websocketUrl, options: {} };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [BrowserModule, ReactiveFormsModule, FormsModule, RouterTestingModule, HttpClientTestingModule],
+      imports: [SocketIoModule.forRoot(config), BrowserModule, ReactiveFormsModule, FormsModule, RouterTestingModule, HttpClientTestingModule],
       declarations: [ ConfirmComponent ]
     })
     .compileComponents();
